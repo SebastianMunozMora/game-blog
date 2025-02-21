b066e6b1b41782290d8d88e6f1dc44e1c42f0ca0
Sebastian <smm9308@gmail.com>
2025-02-21 15:31:41 -0500

    update(rails): from 7.0 to 7.2

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module GameBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,7 +23,5 @@ module GameBlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # 
-    #config.action_controller.default_url_options = { layout: 'application' } 
   end
 end
